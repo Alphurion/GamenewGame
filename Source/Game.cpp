d2f3bbd4eb5e280f37c37994e0c1e28aeb458aee
Alpfury <101234721+Alphurion@users.noreply.github.com>
2024-10-24 21:16:54 -0700

    Update Game.cpp

--- Source/Game.cpp
+++ Source/Game.cpp
@@ -1,1 +1,58 @@
+/*
+This is file is purely meant to call onto game logic
+    #To Do List
 
+- Game
+
+1. Application
+* Game Window
+* Controller or Keyboard input?
+
+2. Player Mechanics
+* Movement
+* Player/Enemy Attacks
+* Health
+* Resources
+
+3. Settings
+* Pause Menu
+* Frame Limiter, vsync
+* Difficulty?
+* Resolution
+
+CONVENTIONS
+newline after function
+void function()
+{
+
+}
+NO SNAKE CASE 
+Bool This_Is_Wrong = false;
+
+USE CAMEL CASE 
+Bool thisIsCorrect = true;
+Classes must be capitalized all the way
+class AllTheWay
+{
+
+};
+
+INITIALIZE ALL VARIABLES BEFORE USING
+int gameData = 0;
+
+ALWAYS using namespace std;
+
+ONLY USE GLOBAL FUNCTIONS IF NECESSARY TO REDUCE MEMORY USAGE
+
+ALWAYS COMMENT REASON FOR USING A VARIABLE
+
+Player mechanics
+Inventory[Row][Column]
+Eqipment [numberOfEqupables]
+
+*/
+
+class GameSettings
+{
+
+};
