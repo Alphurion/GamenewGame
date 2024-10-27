6a4cc35227ec4fc3a6ada11bd29aeac6a5173196
LunarGalaxy <johnnypetrie17@gmail.com>
2024-10-27 16:17:39 -0700

    Comments add to player.gd

--- Source/Godot/Scripts/player.gd
+++ Source/Godot/Scripts/player.gd
@@ -1,25 +1,23 @@
 extends CharacterBody2D
 
-@export var speed = 100
-@export var accel = 100
-var current_dir = "none"
+@export var speed = 100 #speed of the player
+@export var accel = 100 #affects the smoothness of the movement (lower values makes it like walking on ice)
+var current_dir = "none" 
 
 func _ready():
-	$AnimatedSprite2D.play("frontIdle")
+	$AnimatedSprite2D.play("frontIdle") #starts the player off with the idle anim
 
-
-func _physics_process(delta):
-	playerMovement(delta)
-	var moveDirection: Vector2 = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+func _physics_process(_delta): #gets player's input and moves the character accordingly
+	playerMovement()
+	var moveDirection: Vector2 = Input.get_vector("ui_left","ui_right","ui_up","ui_down") 
 	
 	
-	velocity.x = move_toward(velocity.x, speed * moveDirection.x,accel)
+	velocity.x = move_toward(velocity.x, speed * moveDirection.x,accel) #does math stuff to calculate positioning :D
 	velocity.y = move_toward(velocity.y, speed * moveDirection.y,accel)
 	
-	move_and_slide()
-	
+	move_and_slide() #closes the function as a movement function
 
-func playerMovement(delta):
+func playerMovement(): #sets the variable "current_dir" to what is inputted by the player and starts the animation
 	if Input.is_action_pressed("ui_right"):
 		current_dir = "right"
 		playAnim(1)
@@ -34,9 +32,8 @@ func playerMovement(delta):
 		playAnim(1)
 	else:
 		playAnim(0)
-	
-	
-func playAnim(movement):
+
+func playAnim(movement): #chooses what animation to play based off "current_dir" (dir is short for current_dir im just using slang)
 	var dir = current_dir
 	var anim = $AnimatedSprite2D
 	
@@ -67,6 +64,6 @@ func playAnim(movement):
 			anim.play("backWalk")
 		elif movement == 0:
 			anim.play("backIdle")
-		
-func player():
+
+func player(): #passes the script :D
 	pass
