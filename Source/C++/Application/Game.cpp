93d1067bdea4b23fc40297311410e112b4cfb04d
Alpfury <101234721+Alphurion@users.noreply.github.com>
2025-03-02 03:34:14 -0800

    Update Game.cpp

--- Source/C++/Application/Game.cpp
+++ Source/C++/Application/Game.cpp
@@ -1,1 +1,18 @@
+/*
+Game main entry point, calls header files to function with the framerate and all
 
+*/
+
+#include "runtime.h"
+
+
+
+
+int main()
+{
+    Game newGame;
+    newGame.runGame();
+    newGame.endgame();
+
+
+}
