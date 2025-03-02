7996a77d8a338b888782fb5fd6651edbb783bc46
Alpfury <101234721+Alphurion@users.noreply.github.com>
2025-03-02 03:29:54 -0800

    Update runtime.h

--- Source/C++/Application/runtime.h
+++ Source/C++/Application/runtime.h
@@ -1,1 +1,19 @@
+#include "GameSettings.h"
 
+//DO NOT CHANGE
+const float framerate = 24;
+
+class Game
+{
+private:
+        float resolutionW = 1920;
+        float resolutionL = 1080;
+        bool fullscreen = true;
+        bool vsync = false;
+public:
+    void runGame();
+
+    //Constructor Destructor
+    Game();
+    ~Game();
+};
